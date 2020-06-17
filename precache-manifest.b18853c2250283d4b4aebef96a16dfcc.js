928b6d2d9e7cc73355b2fe94bc2996427ff8ef35
mobcoder-ashutosh-patel <ashutosh.patel@mobcoder.com>
2020-06-17 12:32:13 +0530

    Initial commit

---
+++ precache-manifest.b18853c2250283d4b4aebef96a16dfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065c9d4e99c303fa0d2319410bd93f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fae6244de28e3de0a5",
+    "url": "/static/css/main.4d139cad.chunk.css"
+  },
+  {
+    "revision": "7092e9580159d43284b3",
+    "url": "/static/js/2.3a8c4271.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a8c4271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fae6244de28e3de0a5",
+    "url": "/static/js/main.5b0eb6b6.chunk.js"
+  },
+  {
+    "revision": "a0c00e632d9128e82180",
+    "url": "/static/js/runtime-main.37b07460.js"
+  }
+]);
